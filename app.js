1368d12235ea50a110fbb9956a316c1e4bd2705a
terre-plate <alextriq@gmail.com>
2018-02-10 21:32:49 +0100

    OK

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-/**
- * Copyright 2017, Google, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
-// [START app]
-const express = require('express');
+const express = require('express')
+const app = express()
+//app.use(express.static('gs://static-terre'))
+//app.use(express.static('https://storage.googleapis.com/static-terre'))
+app.use(express.static('static')) // A changé pour autre chose (cf au dessus ?) (Non en faite)
 
-const app = express();
+
+const path = require('path')
+var partialsPath=path.join(__dirname+'/views/partials/'); //Required But Why ?
+
+const exphbs = require('express-handlebars')
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  layoutsDir: partialsPath,
+  partialsDir: partialsPath
+}));
+
+app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-  res.status(200).send('<link rel="shortcut icon" type="image/png" href="http://i.omgomg.eu/fes_logo.ico"/>Soyez les bienvenus sur Terre Tiret Plate Dotcom (TPD) bande de fils de putes de touristes intergalactiques !\n<br> JE VOUS DÉTESTE !!!').end();
+  //res.status(200).send('<link rel="shortcut icon" type="image/png" href="http://i.omgomg.eu/fes_logo.ico"/>Soyez les bienvenus sur Terre Tiret Plate Dotcom (TPD) bande de fils de putes de touristes intergalactiques !\n<br> JE VOUS DÉTESTE !!!  <img class="img-responsive" src="gs://static-terre/chaton.png" alt="Blockchain" width="600" />  <img class="img-responsive" src="chaton.jpg" alt="Blockchain" width="600" />').end();
+    res.render('homepage')
 });
 
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+  console.log(`App listening on port ${PORT}`)
+  console.log('Press Ctrl+C to quit (does NOT work on gcloud). ')
 });
 // [END app]
